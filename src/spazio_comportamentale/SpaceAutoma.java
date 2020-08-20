09f8705dc33a072b09eee18fe736af1f81241e42
salvaEbasta <matteosalvalai97@gmail.com>
2020-08-20 20:00:59 +0200

    piccola roba

--- src/spazio_comportamentale/SpaceAutoma.java
+++ src/spazio_comportamentale/SpaceAutoma.java
@@ -23,6 +23,10 @@ public class SpaceAutoma<S extends SpaceState> extends Automa<S, SpaceTransition
 		return acceptingStates;
 	}
 	
+	
+	
+	
+	
 	/**
 	 * Fa SOLO la potatura dell'albero
 	 * @return
