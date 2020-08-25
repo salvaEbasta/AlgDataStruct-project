7ca7dfc54f75b6b86864e5320002507b7a2089a2
salvaEbasta <matteosalvalai97@gmail.com>
2020-08-25 12:21:01 +0200

    Algoritmo completo DiagnosiLineare

---
+++ src/algorithm_interfaces/Algorithm.java
@@ -0,0 +1,21 @@
+package algorithm_interfaces;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class Algorithm<O> implements Callable<O>, MidResult<O> {
+	protected final Logger log;
+	
+	public Algorithm() {
+		log = setupLog();
+	}
+	
+	private Logger setupLog() {
+		Logger log = Logger.getLogger(this.getClass().getSimpleName());
+		System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-7s] %5$s %n");
+		log.setLevel(Level.INFO);
+		//log.addHandler(new StreamHandler(System.out, new SimpleFormatter()));
+		return log;
+	}
+}
--- src/diagnosticatore/DiagnosiLineare.java
+++ src/diagnosticatore/DiagnosiLineare.java
@@ -1,19 +1,105 @@
 package diagnosticatore;
 
-import java.util.concurrent.Callable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
-public class DiagnosiLineare implements Callable<String>{
+import algorithm_interfaces.Algorithm;
+import comportamental_fsm.labels.ObservableLabel;
+import comportamental_fsm.labels.ObservationsList;
+import utility.Constants;
+
+public class DiagnosiLineare extends Algorithm<String>{
 	private ClosureSpace cSpace;
-	private String diagnosis;
+	private ObservationsList linearObs;
+	private StringBuilder R;
 	
-	public DiagnosiLineare(ClosureSpace cSpace) {
+	public DiagnosiLineare(ClosureSpace cSpace, ObservationsList linearObs) {
+		super();
 		this.cSpace = cSpace;
-		this.diagnosis = "";
+		this.linearObs = linearObs;
+		this.R = new StringBuilder();
 	}
 	
 	@Override
 	public String call() throws Exception {
-		throw new Exception();
+		log.info(this.getClass().getSimpleName()+"::call...");
+		
+		//Initialize stringBuilder
+		R.setLength(0);
+		
+		//Initialize X
+		HashMap<SilentClosure, String> X = new HashMap<SilentClosure, String>();
+		X.put(cSpace.initialState(), Constants.EPSILON);
+		
+		//Main cycle
+		for(int i=0; i < linearObs.size(); i++) {
+			ObservableLabel o = linearObs.get(i);
+			
+			log.info("Observable event: "+o);
+			
+			HashMap<SilentClosure, String> X_new = new HashMap<SilentClosure, String>();
+			
+			X.forEach((x, p)->{
+				log.info(String.format("Selected (x:%s, p:%s)", x.id(), p));
+				cSpace.from(x).forEach(t->{
+					
+					// For each <x, (o,p), x2>
+					// x2 = t.sink()
+					if(t.observableLabel().equals(o)) {
+						log.info("Selected transition: "+t);
+						
+						String p2 = p.concat(t.relevantLabelContent());
+						if(X_new.containsKey(t.sink())) {
+							String p2_i = X_new.get(t.sink());
+							X_new.put(t.sink(), "(".concat(p2_i).concat("|").concat(p2).concat(")"));
+						} else 
+							X_new.put(t.sink(), p2);
+						
+						log.info(String.format("New Mapping: (x2:%s, p:%s)", t.sink().id(), X_new.get(t.sink())));
+					}
+				});
+			});
+			
+			X = X_new;
+		}
+
+		//Remove not accepting states
+		log.info("Removing notAccepting states...");
+		
+		Set<SilentClosure> tmp = new HashSet<SilentClosure>(X.keySet());
+		for(SilentClosure s : tmp)
+			if(!s.isAccepting())
+				X.remove(s);
+		
+		//Compose R
+		log.info("Composing R...");
+		
+		Iterator<SilentClosure> iter = X.keySet().iterator();
+		if(X.size() == 1) {
+			SilentClosure x = iter.next();
+			R.append(X.get(x).concat(x.diagnosis()));
+		} else {
+			SilentClosure x = iter.next();
+			R.append("(");
+			R.append(X.get(x).concat("(").concat(x.diagnosis()).concat(")"));
+			R.append(")");
+			while(iter.hasNext()) {
+				x = iter.next();
+				R.append("|(");
+				R.append(X.get(x).concat("(").concat(x.diagnosis()).concat(")"));
+				R.append(")");
+			}
+		}
+		
+		log.info("R: "+R.toString());
+		return R.toString();
+	}
+
+	@Override
+	public String midResult() {
+		return R.toString();
 	}
 	
 	
